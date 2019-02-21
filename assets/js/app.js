9c7d300ae449fed8f350d599d2025e8b1b5ca0cb
Alexander Wolfov <iskander@start2play.ru>
2019-02-21 20:13:39 +0300

    install bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+require('../css/global.scss');
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
 
