70a03526fe577a281db9e54dda62ee63e6987079
Alexander <alesander1996@gmail.com>
2019-02-23 00:27:26 +0300

    fix migration

--- src/Migrations/Version20190222205726.php
+++ src/Migrations/Version20190222205726.php
@@ -23,7 +23,6 @@ final class Version20190222205726 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE route (id INT AUTO_INCREMENT NOT NULL, origin_id VARCHAR(10) NOT NULL, destination_id VARCHAR(10) NOT NULL, INDEX IDX_2C4207956A273CC (origin_id), INDEX IDX_2C42079816C6140 (destination_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE search_request (id INT AUTO_INCREMENT NOT NULL, date_from DATETIME DEFAULT NULL, date_to DATETIME DEFAULT NULL, days_duration_min INT DEFAULT NULL, days_duration_max INT DEFAULT NULL, description VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE route ADD CONSTRAINT FK_2C4207956A273CC FOREIGN KEY (origin_id) REFERENCES city (code)');
         $this->addSql('ALTER TABLE route ADD CONSTRAINT FK_2C42079816C6140 FOREIGN KEY (destination_id) REFERENCES city (code)');
         $this->addSql('ALTER TABLE country CHANGE continent continent VARCHAR(100) DEFAULT NULL');
@@ -35,7 +34,6 @@ final class Version20190222205726 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE route');
-        $this->addSql('DROP TABLE search_request');
         $this->addSql('ALTER TABLE country CHANGE continent continent VARCHAR(100) NOT NULL COLLATE utf8mb4_unicode_ci');
     }
 }
