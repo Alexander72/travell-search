0d97b16df8ea67dc838d9f95ebd42a4708ab0f9a
Alexander Wolfov <iskander@start2play.ru>
2019-02-25 11:22:59 +0300

    incorrect trip options exception

---
+++ src/Exceptions/IncorrectTripOptionsException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 25.02.19
+ * Time: 9:54
+ */
+
+namespace App\Exceptions;
+
+
+use Exception;
+
+class IncorrectTripOptionsException extends Exception
+{
+}
