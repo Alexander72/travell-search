a23909e9902d432e20cba04af61324aba32b347b
Alexander <alesander1996@gmail.com>
2019-03-07 23:19:23 +0300

    start to build search form

--- src/Controller/BuildTripController.php
+++ src/Controller/BuildTripController.php
@@ -10,8 +10,13 @@ namespace App\Controller;
 
 
 use App\Builders\TripBuilder;
+use App\Entity\City;
 use App\Repository\CityRepository;
 use DateTime;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -47,23 +52,42 @@ class BuildTripController extends AbstractController
      */
     public function index(Request $request)
     {
+        $formBuilder = $this->createFormBuilder();
+        $formBuilder->add('startCity', EntityType::class, ['class' => City::class, 'choice_label' => 'name']);
+        $formBuilder->add('finishCity', EntityType::class, ['class' => City::class, 'choice_label' => 'name']);
+        $formBuilder->add('startTime', DateType::class);
+        $formBuilder->add('finishTime', DateType::class);
+        $formBuilder->add('maxPrice', IntegerType::class);
+        $formBuilder->add('maxChanges', IntegerType::class);
+        $formBuilder->add('search', SubmitType::class);
+        $form = $formBuilder->getForm();
+
+
+        return $this->render('buildTripForm.twig', ['form' => $form->createView()]);
+
         $this->cityRepository->findAll();
-        $originCity = $this->cityRepository->find('MOW');
+        $originCity = $this->cityRepository->find('IEV');
         $destinationCity = $this->cityRepository->find('AMS');
         $options = [
             'startCity' => $originCity,
             'finishCity' => $destinationCity,
-            'startTime' => new DateTime('2019-05-01 00:00:00'),
-            'finishTime' => new DateTime('2019-05-08 00:00:00'),
+            'startTime' => new DateTime('2019-05-06 00:00:00'),
+            'finishTime' => new DateTime('2019-05-15 00:00:00'),
             'maxPrice' => 11000,
             'maxChanges' => 5,
         ];
+
 
         $this->tripBuilder->setOptions($options);
         $trips = $this->tripBuilder->buildTrips();
 
         uasort($trips, function($trip1, $trip2){return $trip1->getPrice() <=> $trip2->getPrice();});
+        $trips = \array_slice($trips, 0, 50);
 
+    }
+
+    public function foundedTrips(array $trips)
+    {
         return $this->render('foundedTrips.twig', ['trips' => $trips]);
     }
 }
