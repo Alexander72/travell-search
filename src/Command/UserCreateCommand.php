9d71515fafee37352a7b8dd635dc9df0fdfcfa55
Alexander <alesander1996@gmail.com>
2019-03-12 09:46:19 +0300

    hide user pass in user:create command

--- src/Command/UserCreateCommand.php
+++ src/Command/UserCreateCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -33,7 +34,6 @@ class UserCreateCommand extends Command
         $this->setDescription('Creates a new user.');
         $this->setHelp('This command allows you to create a new user.');
         $this->addArgument('login', InputArgument::REQUIRED);
-        $this->addArgument('password', InputArgument::REQUIRED);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -45,9 +45,17 @@ class UserCreateCommand extends Command
         $user = $userRepository->findOneBy(['login' => $login]);
         if(!$user)
         {
+            $password = $this->getPasswordFromInput("Please enter user password: \n", $input, $output);
+            $passwordConfirm = $this->getPasswordFromInput("Confirm the password: \n", $input, $output);
+
+            if($password !== $passwordConfirm)
+            {
+                throw new \Exception('Passwords do not match');
+            }
+
             $user = new User();
             $user->setLogin($login);
-            $user->setPassword($this->passwordEncoder->encodePassword($user, $input->getArgument('password')));
+            $user->setPassword($this->passwordEncoder->encodePassword($user, $password));
 
             $this->em->persist($user);
             $this->em->flush();
@@ -58,5 +66,31 @@ class UserCreateCommand extends Command
         {
             $io->error("User with login $login already exists.");
         }
+    }
+
+    /**
+     * @param string          $questionMessage
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return mixed
+     */
+    private function getPasswordFromInput(string $questionMessage, InputInterface $input, OutputInterface $output)
+    {
+        $question = new Question($questionMessage);
+        $helper = $this->getHelper('question');
+        $question->setValidator(function($value) {
+            if(trim($value) == '')
+            {
+                throw new \Exception('The password cannot be empty');
+            }
+
+            return $value;
+        });
+        $question->setHidden(true);
+        $question->setMaxAttempts(20);
+        $password = $helper->ask($input, $output, $question);
+
+        return $password;
     }
 }
