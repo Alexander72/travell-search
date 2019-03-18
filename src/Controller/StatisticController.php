56b44aa55cf1afd4572c02616718f67e3bb4f8c5
Alexander <alesander1996@gmail.com>
2019-03-19 02:00:54 +0300

    add charts

--- src/Controller/StatisticController.php
+++ src/Controller/StatisticController.php
@@ -8,16 +8,27 @@
 
 namespace App\Controller;
 
+use App\Repository\LoadFlightsCommandStateRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StatisticController extends AbstractController
 {
+    private $stateRepository;
+
+    public function __construct(
+        LoadFlightsCommandStateRepository $stateRepository
+    ) {
+        $this->stateRepository = $stateRepository;
+    }
+
+
     /**
      * @Route("/admin/statistic", name="statistic")
      */
     public function index()
     {
+        $lastUnfinishedState = $this->stateRepository->getLastState(false);
         return $this->render('admin/statistic.twig');
     }
 }
