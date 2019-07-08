4a1d666d0a9f4944bd139addc15f5408e7332ad9
alexander volkov <iskander@start2play.ru>
2019-07-09 00:31:17 +0300

    fix telegram library

--- src/Controller/TelegramController.php
+++ src/Controller/TelegramController.php
@@ -20,21 +20,26 @@ class TelegramController extends AbstractController
      */
     public function hook(Client $bot, EntityManagerInterface $em)
     {
-        file_put_contents('/motivity/var/log/tg.log', file_get_contents('php://input'), FILE_APPEND);
+        //$result = json_decode(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $s), true);
+
+        //return new Response(var_export($result, 1));
+        file_put_contents('/motivity/var/log/tg.log', "\n".file_get_contents('php://input'), FILE_APPEND);
 
         $bot->command('echo', function(Message $message) use ($bot, $em) {
             $telegramMessage = new TelegramMessage();
             $telegramMessage->setDirection(TelegramMessage::DIRECTION_INCOME);
-            $telegramMessage->setChatId($message->getChat());
+            $telegramMessage->setChatId($message->getChat()->getId());
             $telegramMessage->setText($message->getText());
             $telegramMessage->setDate(new \DateTime());
             $telegramMessage->setMessageId($message->getMessageId());
-            $em->flush($telegramMessage);
+            $em->persist($telegramMessage);
+            $em->flush();
 
             $bot->sendMessage($telegramMessage->getChatId(), $telegramMessage->getText());
         });
 
-        if($data = json_decode(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $bot->getRawBody()), true))
+        $rawBody = $bot->getRawBody();
+        if($data = json_decode(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $rawBody), true))
         {
             $bot->handle([Update::fromResponse($data)]);
         }
