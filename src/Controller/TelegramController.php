35b1c59516d016f5b741acc4f93be880f9533470
alexander volkov <iskander@start2play.ru>
2019-07-08 22:28:25 +0300

    add telegram bot controller

---
+++ src/Controller/TelegramController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use TelegramBot\Api\Client;
+use TelegramBot\Api\Types\Message;
+
+class TelegramController extends AbstractController
+{
+    /**
+     * @Route("/api/v1/telegram/hook", name="api_telegram_hook")
+     */
+    public function hook(Client $bot)
+    {
+        $bot->command('echo', function(Message $message) use ($bot) {
+            $bot->sendMessage($message->getChat()->getId(), $message->getText());
+        });
+
+        $bot->run();
+    }
+}
